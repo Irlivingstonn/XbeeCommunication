8481455c631ec08bf285d8272566b028788fd1fe
Silkinatorr <silkeeonspot@gmail.com>
2025-03-29 15:08:11 -0400

    Updated to read png data

--- coordinator.py
+++ coordinator.py
@@ -17,8 +17,8 @@ dest_16bit = 'FFFE'
 # Covert png to hex
 filename = 'death_star.png'
 with open(filename, 'rb') as f:
-    content = f.read()
-print(binascii.hexlify(content))
+    rf_data = f.read()
+# print(binascii.hexlify(rf_data))
 
 
 
